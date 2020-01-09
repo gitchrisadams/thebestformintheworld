ff9072eec43464f8985373969e28c6a96a49d44f
Sampath Mora <smora@travelclick.com>
2020-01-08 20:02:54 -0500

    added basic form

--- signup_form/src/App.js
+++ signup_form/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form>
+          <h4>First Name</h4>
+          <input></input>
+          <h4>Last Name</h4>
+          <input></input>
+          <h4>Email</h4>
+          <input></input>
+          <h4>Password</h4>
+          <input></input>
+        </form>
+      </div>
     </div>
   );
 }
